3b8b54f21b5c798120daef1678e33bc4c6305378
Thais Gonzalez <thaisgm@umich.edu>
2020-05-14 14:51:01 -0400

    Updates

---
+++ precache-manifest.79f18d7f36e5970698a65d02e5498ba6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83f7142807928fb14ff48ca88d75984",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aba1be110a0d01b241a",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c8d0ab5d5622db4bd9bd",
+    "url": "/static/css/main.b5da6357.chunk.css"
+  },
+  {
+    "revision": "5aba1be110a0d01b241a",
+    "url": "/static/js/2.1290455d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1290455d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8d0ab5d5622db4bd9bd",
+    "url": "/static/js/main.1d4296c8.chunk.js"
+  },
+  {
+    "revision": "997a6e66a946673b0365",
+    "url": "/static/js/runtime-main.e0e41829.js"
+  },
+  {
+    "revision": "d3cd4d32d2d97d1130eba01e9760d6ed",
+    "url": "/static/media/SFB20-FL.d3cd4d32.JPG"
+  },
+  {
+    "revision": "c90b43633a77cb133ab4c144988646c4",
+    "url": "/static/media/backgroundImage.c90b4363.JPG"
+  }
+]);
