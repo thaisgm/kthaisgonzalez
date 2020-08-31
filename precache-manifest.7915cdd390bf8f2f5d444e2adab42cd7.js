8939171e0144d924422f8123f6d16b2dfc7a096e
Thais Gonzalez <thaisgm@umich.edu>
2020-08-30 20:51:14 -0400

    Updates

---
+++ precache-manifest.7915cdd390bf8f2f5d444e2adab42cd7.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780491690132af3fc5d36da8fec4e5bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97851fcb5adab7fa539",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ea9b43255bab13a7b2d2",
+    "url": "/static/css/main.110817c5.chunk.css"
+  },
+  {
+    "revision": "e97851fcb5adab7fa539",
+    "url": "/static/js/2.0899c162.chunk.js"
+  },
+  {
+    "revision": "5d8326d3efa676d3ffe075800bbd250c",
+    "url": "/static/js/2.0899c162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9b43255bab13a7b2d2",
+    "url": "/static/js/main.4efff127.chunk.js"
+  },
+  {
+    "revision": "b3eaf296f1b15d481c1c",
+    "url": "/static/js/runtime-main.4fa27d06.js"
+  },
+  {
+    "revision": "4669e1d1acd60e205c292247f558a285",
+    "url": "/static/media/BBHome.4669e1d1.png"
+  },
+  {
+    "revision": "3d1cd2802891f331bb0e1c992a126285",
+    "url": "/static/media/BBLogin.3d1cd280.png"
+  },
+  {
+    "revision": "38aea69298d8545e2ffc032cde7f4b66",
+    "url": "/static/media/BBProfile.38aea692.png"
+  },
+  {
+    "revision": "29be4db85f19c81f4f6e3947f5a9447d",
+    "url": "/static/media/BBSearch.29be4db8.png"
+  },
+  {
+    "revision": "c084a6f564508a84d77aa6816d458988",
+    "url": "/static/media/BBSearchResult.c084a6f5.png"
+  },
+  {
+    "revision": "4753c8d237a064094f4e398d99f0ad07",
+    "url": "/static/media/BingeBuddyLogo.4753c8d2.png"
+  },
+  {
+    "revision": "a7ccb4268483e4bd7a8354ecc2ee86da",
+    "url": "/static/media/GIThackteam.a7ccb426.jpg"
+  },
+  {
+    "revision": "654a3dcda76c723488cff025b50b22da",
+    "url": "/static/media/GifterDemohome.654a3dcd.png"
+  },
+  {
+    "revision": "b5efadfc28237b42733ed0612cca7d9b",
+    "url": "/static/media/GifterMainScreenDemo.b5efadfc.png"
+  },
+  {
+    "revision": "07883e93734b98cae0f7b9c55d287250",
+    "url": "/static/media/GithubLogo.07883e93.svg"
+  },
+  {
+    "revision": "1f878d070af73877c6a7ebbe8beb8639",
+    "url": "/static/media/HomePageSpanish.1f878d07.png"
+  },
+  {
+    "revision": "b4ce76398e5ce0705a8bc19db82868de",
+    "url": "/static/media/IMG_0380.b4ce7639.jpg"
+  },
+  {
+    "revision": "7e193250c1d756440cd00eba8e8ae7af",
+    "url": "/static/media/QuizPage.7e193250.png"
+  },
+  {
+    "revision": "d3cd4d32d2d97d1130eba01e9760d6ed",
+    "url": "/static/media/SFB20-FL.d3cd4d32.JPG"
+  },
+  {
+    "revision": "b2126d5a1fd5540cdf9e21b6769043af",
+    "url": "/static/media/SFCity.b2126d5a.png"
+  },
+  {
+    "revision": "143bd0d0200028df700d7d89c56b5ddd",
+    "url": "/static/media/SFDiagram.143bd0d0.png"
+  },
+  {
+    "revision": "4014a5e25548b3f24284aa17c435169d",
+    "url": "/static/media/School Results.4014a5e2.png"
+  },
+  {
+    "revision": "c90b43633a77cb133ab4c144988646c4",
+    "url": "/static/media/backgroundImage.c90b4363.JPG"
+  },
+  {
+    "revision": "27ee8dfaae1e2b1f8d56ba8bc17fa79f",
+    "url": "/static/media/fuzzies.27ee8dfa.png"
+  },
+  {
+    "revision": "3811a7f9d3342274b1e3f0a867c7e173",
+    "url": "/static/media/gifterLogo.3811a7f9.png"
+  },
+  {
+    "revision": "097b77494452f9352630bcf79697db4d",
+    "url": "/static/media/instagramLogo.097b7749.png"
+  },
+  {
+    "revision": "884da9b8f19763199e62e0893702d0ed",
+    "url": "/static/media/linkedinLogo.884da9b8.png"
+  },
+  {
+    "revision": "438eca5cf3d8ca6fb5bc6cccd3d6f14d",
+    "url": "/static/media/thaisCartoon.438eca5c.jpg"
+  },
+  {
+    "revision": "879cd5cfe65655c53b445a0852cb5fdb",
+    "url": "/static/media/twillio.879cd5cf.png"
+  }
+]);
